02b219f8f4d890089bb5aea6435d5a9e3c6a3df1
dunnkers <jeroenoverschie@gmail.com>
2018-11-14 16:18:38 +0100

    Commit to using Ruby 💎

--- main.rb
+++ main.rb
@@ -1,15 +1,14 @@
 require 'Goodreads'
 
 client = Goodreads::Client.new(
-    api_key: "GbOjze9tMkWgYYTuKHrDtA", api_secret: "VUmugs2TnBzxnZqzURRd9rockt6HE5kyO3DgWnzQ"
+    api_key: "GbOjze9tMkWgYYTuKHrDtA",
+    api_secret: "VUmugs2TnBzxnZqzURRd9rockt6HE5kyO3DgWnzQ"
     )
 
 user_id = 88771324
 shelf_name = "currently-reading"
 shelf = client.shelf(user_id, shelf_name)
 
-print shelf.books  # array of books on this shelf
-
 for book in shelf.books do
-    print book.title
+    print book.book.title + "\n"
 end
