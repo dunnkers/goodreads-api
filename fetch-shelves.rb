d2d5447dfc8770ff3d298ba0621f368d78d0b08d
dunnkers <jeroenoverschie@gmail.com>
2019-02-14 22:50:15 +0100

    Use environment variables instead of hard-coded keys 💎

--- fetch-shelves.rb
+++ fetch-shelves.rb
@@ -4,13 +4,19 @@ require 'open-uri'
 require 'redis'
 require 'json'
 
-$redis = Redis.new
+GOODREADS_API_KEY = ENV["GOODREADS_API_KEY"]
+GOODREADS_USER_ID = ENV["GOODREADS_USER_ID"]
 
-$client = Goodreads::Client.new(
-    api_key: "GbOjze9tMkWgYYTuKHrDtA",
-    api_secret: "VUmugs2TnBzxnZqzURRd9rockt6HE5kyO3DgWnzQ"
-    )
-USER_ID = 88771324
+if not GOODREADS_API_KEY
+    abort "Error: Environment variable `GOODREADS_API_KEY` not set."
+end
+
+if not GOODREADS_USER_ID
+    abort "Error: Environment variable `GOODREADS_USER_ID` not set."
+end
+
+$redis = Redis.new
+$client = Goodreads::Client.new(api_key: GOODREADS_API_KEY)
 
 def grabBookCover(bookLink)
     doc = Nokogiri::HTML(open(bookLink))
@@ -46,9 +52,9 @@ def fixBookCover(book)
 end
 
 def fetchShelves
-    read = $client.shelf(USER_ID, "read")
+    read = $client.shelf(GOODREADS_USER_ID, "read")
     read.books = read.books.map { |book| fixBookCover(book) }
-    current = $client.shelf(USER_ID, "currently-reading")
+    current = $client.shelf(GOODREADS_USER_ID, "currently-reading")
     current.books = current.books.map { |book| fixBookCover(book) }
     shelves = {
         "read" => read,
