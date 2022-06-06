75149b8e490c415f9380244af4b2ad4f21e11f5f
Jeroen Overschie <jeroenoverschie@gmail.com>
2022-06-06 17:19:24 +0200

    Remove unused code

--- app.rb
+++ app.rb
@@ -7,7 +7,6 @@ FunctionsFramework.on_startup do
     fetchShelvesOrUseCache()
 end
 
-
 class App < Sinatra::Base
   get "/" do
     content_type :json
@@ -24,32 +23,6 @@ end
 FunctionsFramework.http "get-shelves" do |request|
   App.call request.env
 end
-
-
-# FunctionsFramework.http("get-shelves") do |request|
-#     if request.options?
-#         headers = {
-#             "Access-Control-Allow-Origin"  => "*",
-#             "Access-Control-Allow-Methods" => "GET",
-#             "Access-Control-Allow-Headers" => "Content-Type",
-#             "Access-Control-Max-Age"       => "3600"
-#         }
-#         return [204, headers, []]
-#     else
-#         headers = {
-#             "Access-Control-Allow-Origin" => "*",
-#             "Content-Type" => "application/json"
-#         }
-
-#         bust_str = request.params["bust"] rescue "false"
-#         bust = bust_str == "true"
-#         shelves = fetchShelvesOrUseCache(bust: bust)
-#         puts(shelves)
-#         shelves_json = JSON.generate(shelves)
-
-#         return [200, headers, shelves_json]
-#     end
-# end
 
 FunctionsFramework.cloud_event "force-get-shelves" do |event|
     logger.info "force-get-shelves pub/sub cloud event was triggered."
